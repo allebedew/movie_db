507747cf726d733289160bbc45f6f0778141c50e
Alex Lebedev <allebedew@gmail.com>
2022-04-19 14:07:56 +0300

    initial

---
+++ MovieDB/MovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBApp.swift
+//  MovieDB
+//
+//  Created by Alex on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
